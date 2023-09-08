bdd516f116ac5413dbe7579ee7c695a0f8720ea1
Eduardo Pedroso <andreeduardopp@gmail.com>
2023-09-08 20:13:44 -0300

    topzera

--- ProjetoSemeando/Eventos/views.py
+++ ProjetoSemeando/Eventos/views.py
@@ -3,17 +3,22 @@ from rest_framework import serializers
 from rest_framework import viewsets
 from Eventos.models import Evento, Categoria
 from rest_framework import filters
-from django.db.models import Q
+from datetime import date
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.permissions import SAFE_METHODS
 from Usuarios.models import User
+from rest_framework.pagination import PageNumberPagination
 
 class OrganizadorSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name')
 
+class CustomPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
@@ -55,8 +60,15 @@ class EventoView(viewsets.ModelViewSet):
     queryset = Evento.objects.all() 
     serializer_class = EventoReadSerializer  
     http_method_names = ['get', 'post', 'put', 'delete']
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend,filters.OrderingFilter]  
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend,filters.OrderingFilter] 
     search_fields = ['titulo', 'descricao']
+    pagination_class = CustomPagination
+
+    def get_queryset(self):
+        current_date = date.today()
+        
+        # Filtrar os eventos com datas maiores ou iguais à data atual
+        return Evento.objects.filter(data__gte=current_date)
     
     def get_serializer_class(self):
         if self.request.method in SAFE_METHODS:
