5d4403914b1785c6f8e81104869cfe3301e43ab4
Eduardo Pedroso <andreeduardopp@gmail.com>
2023-09-13 23:07:46 -0300

    TRU

--- ProjetoSemeando/Usuarios/adapter.py
+++ ProjetoSemeando/Usuarios/adapter.py
@@ -1,8 +1,9 @@
 from allauth.account.adapter import DefaultAccountAdapter
-
 from ProjetoSemeando.settings import URL_FRONTEND
+from dj_rest_auth.serializers import PasswordResetSerializer
 
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
         return f'{URL_FRONTEND}/confirm-email/{emailconfirmation.key}'
+
